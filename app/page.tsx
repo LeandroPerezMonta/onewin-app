e80bd884ae81f33bff71470556e677be368bcad6
Anderson Lopez <anderson.lopez@andeanwide.com>
2024-12-04 06:48:39 -0500

    Fix-styles

--- app/page.tsx
+++ app/page.tsx
@@ -50,11 +50,44 @@ export default function Home() {
       <div className={`pb-8 pt-4 w-full ${currentUser ? 'mb-[18rem]' : ''}`}>
         {currentUser ? (
           <div className="mt-8">
-            <div className="w-full justify-center text-center"><h1 className="text-2xl font-bold mb-4">Metodos de Pago</h1></div>
+            <div className="w-full justify-center text-center "><h1 className="text-3xl font-bold mb-4 bg-gradient-to-r from-[#ececec] to-[#5d28cf]  bg-clip-text text-transparent font-inter">Recarga tu saldo</h1></div>
             <div className="w-full justify-center text-center"><h1 className="text-xl font-bold mb-4"></h1>Se te Retornará de 200% de tu Recarga 🥳</div>
-            <Accordion>
-            <AccordionItem key="1" aria-label="Accordion 1" title="USDT Wallet">
-              <div className="flex flex-col items-center w-full ">
+            <Accordion defaultSelectedKeys={["1", "2", "3"]}>
+            <AccordionItem key="1" aria-label="Accordion 1" title="USDT Wallet" >
+            <div className="flex flex-col items-center w-full text-white ">
+                <Image src={"/QrNequi.jpg"} alt="" width={300} height={300} />
+                <div
+                  className="my-4"
+                  onClick={() => {
+                    toast.success("Wallet copiada");
+                  }}
+                >
+                  <h1 className="font-bold mb-2">Por Nequi:</h1>
+                </div>
+              </div>
+              
+            </AccordionItem>
+            <AccordionItem
+              key="2"
+              aria-label="Accordion 2"
+              title="Link de Trust"
+            >
+              <div className="my-4 flex text-2xl flex-col w-full items-center text-white">
+                <h1 className="font-bold mb-2">Mi Wallet de USDT:</h1>
+                <Link
+                  href={
+                    "https://link.trustwallet.com/send?coin=20000714&address=0xFb3eDCA7041FD5E8F9e09F652C5f06bBa1D725Ba&token_id=0x55d398326f99059fF775485246999027B3197955"
+                  }
+                  className="font-semibold hover:underline text-blue-600 flex items-center space-x-2"
+                  target="_blank"
+                >
+                  <h1 className="">Link</h1>
+                  <BsArrowRight />
+                </Link>
+              </div>
+            </AccordionItem>
+            <AccordionItem key="3" aria-label="Accordion 3" title="Nequi">
+            <div className="flex flex-col items-center w-full ">
                 <Image
                   src={"/qrWalletUsdt.jpg"}
                   alt=""
@@ -62,7 +95,7 @@ export default function Home() {
                   height={300}
                 />
                 <div
-                  className="my-4 text-center"
+                  className="my-4 text-center text-white"
                   onClick={() => {
                     toast.success("Wallet copiada");
                   }}
@@ -83,38 +116,6 @@ export default function Home() {
                       </Tooltip>
                     </div>
                   </CopyToClipboard>
-                </div>
-              </div>
-            </AccordionItem>
-            <AccordionItem
-              key="2"
-              aria-label="Accordion 2"
-              title="Link de Trust"
-            >
-              <div className="my-4 flex text-2xl flex-col w-full items-center">
-                <h1 className="font-bold mb-2">Mi Wallet de USDT:</h1>
-                <Link
-                  href={
-                    "https://link.trustwallet.com/send?coin=20000714&address=0xFb3eDCA7041FD5E8F9e09F652C5f06bBa1D725Ba&token_id=0x55d398326f99059fF775485246999027B3197955"
-                  }
-                  className="font-semibold hover:underline text-blue-600 flex items-center space-x-2"
-                  target="_blank"
-                >
-                  <h1 className="">Link</h1>
-                  <BsArrowRight />
-                </Link>
-              </div>
-            </AccordionItem>
-            <AccordionItem key="3" aria-label="Accordion 3" title="Nequi">
-              <div className="flex flex-col items-center w-full ">
-                <Image src={"/QrNequi.jpg"} alt="" width={300} height={300} />
-                <div
-                  className="my-4"
-                  onClick={() => {
-                    toast.success("Wallet copiada");
-                  }}
-                >
-                  <h1 className="font-bold mb-2">Por Nequi:</h1>
                 </div>
               </div>
             </AccordionItem>
