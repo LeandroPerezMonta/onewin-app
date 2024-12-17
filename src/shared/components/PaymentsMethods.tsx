097df7f36f33f829da435a6f5a177bc31a4b1df6
Anderson Lopez <anderson.lopez@andeanwide.com>
2024-12-17 10:30:36 -0500

    Add-wallet-copied

--- src/shared/components/PaymentsMethods.tsx
+++ src/shared/components/PaymentsMethods.tsx
@@ -1,35 +1,52 @@
 "use client";
 
-import { Card, CardBody, Modal, ModalBody, ModalContent, ModalHeader } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Tooltip,
+} from "@nextui-org/react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { useState } from "react";
+import copy from "copy-to-clipboard";
+import { toast } from "react-toastify";
 
 const banks = [
   {
     name: "Bancolombia",
     logo: "/bancolombia.png",
-    qr: "/bancolombiaQR.jpg"
+    qr: "/bancolombiaQR.jpg",
+    footer: "",
   },
   {
     name: "Nequi",
     logo: "/nequi.png",
-    qr: "/nequiQR.jpg"
+    qr: "/nequiQR.jpg",
+    footer: "",
   },
   {
     name: "Davivienda",
     logo: "/davivienda.png",
-    qr: "/daviviendaQR.jpg"
+    qr: "/daviviendaQR.jpg",
+    footer: "",
   },
   {
     name: "Daviplata",
     logo: "/daviplata.png",
-    qr: "/daviplataQR.jpg"
+    qr: "/daviplataQR.jpg",
+    footer: "",
   },
   {
     name: "Usdt",
     logo: "/usdt.png",
-    qr: "/usdtQR.jpg"
+    qr: "/usdtQRcopia.jpg",
+    footer: "TU2MTgZ6PtAG8Lu6WSbJ9aDPmHU2hgCJZY",
   },
 ];
 
@@ -54,17 +71,38 @@ const item = {
 };
 
 export default function PaymentMethods() {
-  const [selectedBank, setSelectedBank] = useState<{ name: string; logo: string; qr: string } | null>(null);
+  const [isCopied, setIsCopied] = useState(false);
+  const [selectedBank, setSelectedBank] = useState<{
+    name: string;
+    logo: string;
+    qr: string;
+    footer: string;
+  } | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const openModal = (bank: { name: string; logo: string; qr: string }) => {
+  const openModal = (bank: {
+    name: string;
+    logo: string;
+    qr: string;
+    footer: string;
+  }) => {
     setSelectedBank(bank);
     setIsModalOpen(true);
+    setIsCopied(false);
   };
 
   const closeModal = () => {
     setSelectedBank(null);
     setIsModalOpen(false);
+  };
+
+  const handleCopy = () => {
+    if (selectedBank?.footer) {
+      toast.success("Wallet copiado al portapapeles");
+      copy(selectedBank.footer);
+      setIsCopied(true);
+      setTimeout(() => setIsCopied(false), 2000);
+    }
   };
 
   return (
@@ -77,7 +115,11 @@ export default function PaymentMethods() {
       >
         {banks.map((bank) => (
           <motion.div key={bank.name} variants={item}>
-            <Card isPressable className="w-full" onClick={() => openModal(bank)}>
+            <Card
+              isPressable
+              className="w-full"
+              onClick={() => openModal(bank)}
+            >
               <CardBody className="flex flex-row items-center gap-3 p-4 bg-gradient-to-r from-[#1e283f] to-[#0f1627] text-white">
                 <motion.div
                   className="relative h-10 w-10 overflow-hidden rounded-md bg-background"
@@ -114,6 +156,19 @@ export default function PaymentMethods() {
                 className="object-contain"
               />
             </ModalBody>
+            <ModalFooter className="flex w-full justify-center flex-col gap-2">
+              {selectedBank.footer && (
+                <Tooltip
+                  content={isCopied ? "¡Copiado!" : "Copiar"}
+                  color="default"
+                  showArrow
+                >
+                  <Button onClick={handleCopy}>
+                    {selectedBank.footer || "Sin información"}
+                  </Button>
+                </Tooltip>
+              )}
+            </ModalFooter>
           </ModalContent>
         </Modal>
       )}
